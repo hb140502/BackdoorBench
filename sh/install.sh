fcb35b24e9be7fa0e919c85e977eb486c7ec1fbc
Henk <hb130502@outlook.com>
2025-03-05 16:03:09 +0100

    Certain flags not supported in requirements.txt, so adjust install.sh instead

--- sh/install.sh
+++ sh/install.sh
@@ -1,38 +1,38 @@
-pip install torch==1.11+cu113 torchvision==0.12 torchaudio==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html
-pip install keras==2.7.0
-pip install opencv-python==4.5.5.64
-pip install pandas==1.3.1
-pip install Pillow==8.2.0
-pip install scikit-learn==0.24.2
-pip install scikit-image==0.18.1
-pip install tqdm==4.61.0
-pip install pyyaml==5.4.1
-pip install tensorboard==2.7.0
-pip install Kornia==0.5.0
-pip install imageio==2.18.0
-pip install matplotlib==3.5.1
-pip install scipy==1.3.1
+pip install --no-cache-dir torch==1.11+cu113 torchvision==0.12 torchaudio==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install --no-cache-dir keras==2.7.0
+pip install --no-cache-dir opencv-python==4.5.5.64
+pip install --no-cache-dir pandas==1.3.1
+pip install --no-cache-dir Pillow==8.2.0
+pip install --no-cache-dir scikit-learn==0.24.2
+pip install --no-cache-dir scikit-image==0.18.1
+pip install --no-cache-dir tqdm==4.61.0
+pip install --no-cache-dir pyyaml==5.4.1
+pip install --no-cache-dir tensorboard==2.7.0
+pip install --no-cache-dir Kornia==0.5.0
+pip install --no-cache-dir imageio==2.18.0
+pip install --no-cache-dir matplotlib==3.5.1
+pip install --no-cache-dir scipy==1.3.1
 
-# for visualization only
-pip install seaborn==0.11.2
-## Shapely Value
-pip install shap==0.40.0
-## Grad-CAM
-pip install grad-cam==1.3.9
-## Feature Map & Feature Visualization
-pip install omnixai==1.2.3
-pip install plotly==5.11.0
-## UMAP
-pip install umap-learn==0.5.3
-## Network Structure
-pip install graphviz
-pip install hiddenlayer==0.3
-pip install -U git+https://github.com/szagoruyko/pytorchviz.git@master
+# # for visualization only
+# pip install --no-cache-dir seaborn==0.11.2
+# ## Shapely Value
+# pip install --no-cache-dir shap==0.40.0
+# ## Grad-CAM
+# pip install --no-cache-dir grad-cam==1.3.9
+# ## Feature Map & Feature Visualization
+# pip install --no-cache-dir omnixai==1.2.3
+# pip install --no-cache-dir plotly==5.11.0
+# ## UMAP
+# pip install --no-cache-dir umap-learn==0.5.3
+# ## Network Structure
+# pip install --no-cache-dir graphviz
+# pip install --no-cache-dir hiddenlayer==0.3
+# pip install --no-cache-dir -U git+https://github.com/szagoruyko/pytorchviz.git@master
 
-## Landscape
-pip install PyHessian==0.1
+# ## Landscape
+# pip install --no-cache-dir PyHessian==0.1
 
-## Quality
-pip install torchmetrics[image]
+# ## Quality
+# pip install --no-cache-dir torchmetrics[image]
 
-pip install pytorch-wavelets
+# pip install --no-cache-dir pytorch-wavelets
