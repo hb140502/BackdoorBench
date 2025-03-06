117de7ce96f2dad0ca087239236395f0d1ffa930
Henk <hb130502@outlook.com>
2025-03-06 12:24:36 +0100

    Modify to run on local machine

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 repo_dir="$HOME/BackdoorBench"
-my_dir="/vol/csedu-nobackup/project/hberendsen"
+my_dir=$repo_dir
 data_dir="$my_dir/data"
 record_dir="$my_dir/record"
 timestamp=$(date +"%d-%m_%H:%M")
 
-python ./attack/badnet.py --yaml_path "$repo_dir/config/attack/prototype/cifar10.yaml" --patch_mask_path "$repo_dir/resource/badnet/trigger_image.png"  --save_parent_dir "$record_dir" --save_folder_name "badnet_$timestamp" --dataset_path="$data_dir" --epochs 1
+python ./attack/badnet.py --yaml_path "$repo_dir/config/attack/prototype/cifar10.yaml" --patch_mask_path "$repo_dir/resource/badnet/trigger_image.png"  --save_parent_dir "$record_dir" --save_folder_name "badnet_$timestamp" --dataset_path="$data_dir" --epochs 1 --device cpu
