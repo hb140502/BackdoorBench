4783c6df2f3d1f92aa1370db37446200f2936c54
Henk <hb130502@outlook.com>
2025-03-08 12:38:26 +0100

    Add weights_only=False required to use load_attack_result in newer pytorch versions

--- utils/save_load_attack.py
+++ utils/save_load_attack.py
@@ -195,7 +195,7 @@ def load_attack_result(
     save_path MUST have 'record' in its abspath, and data_path in attack result MUST have 'data' in its path!!!
     save_path : the path of "attack_result.pt"
     '''
-    load_file = torch.load(save_path)
+    load_file = torch.load(save_path, weights_only=False)
 
     if all(key in load_file for key in ['model_name',
         'num_classes',
