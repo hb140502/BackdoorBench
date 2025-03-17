57b2ce71c85a52ab04ef590b5964193022e0ff81
Henk <hb130502@outlook.com>
2025-03-17 18:22:43 +0100

    Distinguish between bd and cross samples in WaNet

--- attack/wanet.py
+++ attack/wanet.py
@@ -537,6 +537,7 @@ class Wanet(BadNet):
                         img=(t_img),
                         bd_label=int(target_changed[idx_in_batch]),
                         label=int(targets[idx_in_batch]),
+                        indicator=1 if idx_in_batch < num_bd else 2
                     )
 
         save_attack_result(
