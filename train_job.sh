79e4eb58bd87839338f5b4acb03347183449774c
Henk Berendsen <hberendsen@cn84.science.ru.nl>
2025-03-18 12:57:05 +0100

    Make model, dataset and pratio variable

--- train_job.sh
+++ train_job.sh
@@ -5,11 +5,11 @@
 #SBATCH -c 4 
 #SBATCH --mem 4G 
 #SBATCH --gres=gpu:rtx_2080_ti:1 
-#SBATCH -t 2:00:00
+#SBATCH -t 3:00:00
 #SBATCH --output=jobs/%j.out
 #SBATCH --error=jobs/%j.err
 
 . ./input_validation.sh
 input_validation $@
 
-./train.sh $attack $n_epochs
+./train.sh $attack $model $dataset $pratio $n_epochs
