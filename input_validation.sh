c0bede74598dac37f535f95361a9e737fc796fe5
Henk Berendsen <hberendsen@cn84.science.ru.nl>
2025-03-18 12:57:05 +0100

    Make model, dataset and pratio variable

--- input_validation.sh
+++ input_validation.sh
@@ -2,13 +2,41 @@
 
 function input_validation() {
     usage_str="""
-Usage: ${0} <ATTACK> <NUMBER OF EPOCHS>"""
+Usage: ${0} <ATTACK> <MODEL ARCHITECTURE> <DATASET> <POISON RATE> <NUMBER OF EPOCHS>"""
 
-    if [[ $# -ne 2 ]]; then
+    if [[ $# -ne 5 ]]; then
         echo $usage_str
         exit 1
     fi
 
     attack=$1
-    n_epochs=$2
+    model=$2
+    dataset=$3
+    pratio=$4
+    n_epochs=$5
+
+    attack_regex="^prototype|badnet|blended|wanet|bpp$"
+    validate_str $attack $attack_regex
+
+    model_regex="^resnet18|vgg16|densenet121$"
+    validate_str $model $model_regex
+
+    dataset_regex="^cifar10|cifar100|tiny$"
+    validate_str $dataset $dataset_regex
+
+    pratio_regex="^0.003|0.05|0.1|None$" # Last option for clean model
+    validate_str $pratio $pratio_regex
+
+    n_epochs_regex="^[0-9]+$"
+    validate_str $n_epochs $n_epochs_regex
+}
+
+function validate_str() {
+    str=$1
+    regex=$2
+
+    if [[ ! $str =~ $regex ]]; then
+        echo "Argument $str is invalid"
+        exit 1
+    fi
 }
--- train.sh
+++ train.sh
@@ -7,24 +7,24 @@ repo_dir="$HOME/master-thesis/code/backdoorbench"
 my_dir="/vol/csedu-nobackup/project/hberendsen"
 data_dir="$my_dir/data"
 record_dir="$my_dir/record"
-timestamp=$(date +"%d-%m_%H:%M")
+timestamp=$(date +"T%d-%m_%H-%M")
 
-model_regex="^prototype|badnet|blended|wanet|bpp$"
+# gpu=$(python get_gpu.py)
 
-if [[ ! $attack =~ $model_regex ]]; then
-    echo "Attack $attack is not supported"
-    exit 1
-fi
+# if [[ ! $gpu =~ "RTX 2080 Ti" ]]; then
+#     echo "Unexpected GPU: ${gpu}"
+#     exit 1
+# fi
 
-attack_id="${attack}_${timestamp}"
+pratio_label=$(echo p$pratio | tr . -)
+attack_id="${attack}_${model}_${dataset}_${pratio_label}"
 
 yaml_conf="--yaml_path config/attack/custom/cifar10.yaml"
 
 # Add additional attack-specific configuration unless attack == prototype (clean model)
 if [[ $attack != "prototype" ]]; then
     yaml_conf="${yaml_conf} --bd_yaml_path config/attack/custom/$attack.yaml"
-    attack_opts="--attack_target 0 --pratio 0.05"
+    attack_opts="--attack_target 0 --pratio $pratio"
 fi
 
-# TODO: make pratio, dataset and model variable
-python ./attack/$attack.py $yaml_conf $attack_opts --save_parent_dir "$record_dir" --save_folder_name $attack_id  --dataset_path="$data_dir" --model resnet18 --dataset cifar10 --epochs $n_epochs --device cpu
+python ./attack/$attack.py $yaml_conf $attack_opts --save_parent_dir "$record_dir" --save_folder_name $attack_id  --dataset_path="$data_dir" --model $model --dataset $dataset --epochs $n_epochs --device cpu
